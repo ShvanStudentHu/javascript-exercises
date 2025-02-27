366bdcfa9d3b908e60efa305bec53ed2ffffa53e
Shvan <shvan.yakana@student.hu.nl>
2025-02-27 14:56:41 +0100

    helloworld done repeat string doen

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  const error = "ERROR";
+  if (num < 0) {
+    return error;
+  }
+  let finalResult = "";
+  for (let i = 0; i < num; i++) {
+    finalResult += string;
+  }
+  return finalResult;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+//declare string to a variable
+//add the string to the previous (result)string
+//print out the final result
+//do this x amout of times
+//if num is negative
+//return error
