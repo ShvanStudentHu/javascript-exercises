bada8ae90b908e7f67209f40825713eb168e1de2
Shvan <shvan.yakana@student.hu.nl>
2025-03-03 11:48:47 +0100

    excersice 4 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,20 +1,18 @@
-const removeFromArray = function (array, removeNumber) {
-  //store the length of the array
-  //loop through the array to check if a number in the array === the number that needs to be removed
-  //if the number is found store the index of the number in a variable
-  //use toSpliced to remove the number
-  //continue to the next number
-  let arrayLength = array.length;
-  let numbersInArray = removeNumber.length;
-  let newArray;
-  for (let i = 0; i < arrayLength; i++) {
-    // [3,2]
-    if (array[i] === removeNumber) {
-      newArray = array.toSpliced(i, 1);
+const removeFromArray = function (array, ...removeNumbers) {
+  for (let i = 0; i < removeNumbers.length; i++) {
+    for (let y = array.length - 1; y >= 0; y--) {
+      if (removeNumbers[i] === array[y]) {
+        array.splice(y, 1);
+      }
     }
   }
-  return newArray;
+  return array;
 };
+//store the length of the array
+//loop through the array to check if a number in the array === the number that needs to be removed
+//if the number is found store the index of the number in a variable
+//use toSpliced to remove the number
+//continue to the next number decrement to avoide indexing issues
 
 // Do not edit below this line
 module.exports = removeFromArray;
