86c0920fcca1dee224b7c446e76224a75d6dffc2
Shvan <shvan.yakana@student.hu.nl>
2025-03-04 18:04:28 +0100

    excersise done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  if (Number.isInteger(year / 4)) {
+    if (Number.isInteger(year / 100) && Number.isInteger(year / 400)) {
+      return true;
+    } else if (!Number.isInteger(year / 400) && Number.isInteger(year / 100)) {
+      return false;
+    }
+    return true;
+  }
 
+  return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+//if number is deviseble by 4 it could be a leap year
+//if the number is devisabe by 100 it could not be a leap year
+//if number is deviseble by 400 it is a leap year
