2ef2852366380a50fbadb99c0174908bda3525be
Shvan <shvan.yakana@student.hu.nl>
2025-03-05 09:45:06 +0100

    tempConversion Done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function (F) {
+  F = (F - 32) * (5 / 9);
 
-const convertToFahrenheit = function() {
+  return Math.round(F * 10) / 10;
+};
+const convertToFahrenheit = function (C) {
+  C = C * (9 / 5) + 32;
+  return Math.round(C * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
+
+// x = (x * 9/5 + 32)
