bcd4806b2c902dea1b443596b8f4638cd9a4b649
Shvan <shvan.yakana@student.hu.nl>
2025-03-19 07:19:46 +0100

    excersice 08 finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function (numOne, numTwo) {
+  return (result = numOne + numTwo);
 };
 
-const subtract = function() {
-	
+const subtract = function (numOne, numTwo) {
+  return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let total = array.reduce((num, nextNum) => {
+    return num + nextNum;
+  }, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let total = array.reduce((num, nextNum) => {
+    return num * nextNum;
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function (numOne, numTwo) {
+  return numOne ** numTwo;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let finalResult;
+  const array = [];
+  if (num == 0) {
+    return 1;
+  }
+
+  for (let i = 1; i <= num; i++) {
+    array.push(i);
+  }
+
+  const result = multiply(array);
+  return result;
+
+  // 1 x 2 = (antwoord) x 3 = (antwoord) x 4
+  // 2 x 3 = 6
+  // 3 x 4 = 12
+  // 3 x 4 = 12
 };
 
 // Do not edit below this line
@@ -29,5 +51,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
